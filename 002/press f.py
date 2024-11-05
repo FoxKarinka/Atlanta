f74fd88c603cbc49d486db2070b233612df35634
FoxKarinka <justdayrelax@gmail.com>
2024-11-05 14:48:34 +0200

    new new papka

---
+++ 002/press f.py
@@ -0,0 +1,11 @@
+while True:
+    msg=input("Please input msg ")
+    if msg=="Добрий день":
+        print("Добрий день, бро")
+    elif msg=="Слава Ісусу Христу":
+        print("Навікі слава")
+    elif msg=="Слава Україні":
+        print("Героям слава")
+    elif msg=="Па-па":
+        print("The end")
+        break
